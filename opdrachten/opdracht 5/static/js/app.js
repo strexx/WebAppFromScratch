2a28ecc07546023a3d2064e08276948845931b83
strexx <ffh_93@live.nl>
2016-02-05 15:57:15 +0100

    Update app.js

--- opdrachten/opdracht 5/static/js/app.js
+++ opdrachten/opdracht 5/static/js/app.js
@@ -2,12 +2,7 @@
 	'use strict';
 
 	var main = document.querySelector('main');
-
-<<<<<<< HEAD
-	var app = {
-=======
 	var APP = {
->>>>>>> master
 		init: function() {
 
 			routes.init();
@@ -18,44 +13,31 @@
 	// Routing
 	var routes = {
 		init: function() {
-<<<<<<< HEAD
 
-			// haschanged event listener
-			window.addEventListener('hashchange', sections.toggle ,false);
-			// also check the page on load
-			window.addEventListener('load', sections.toggle ,false);
-
-=======
 			window.addEventListener('hashchange', sections.toggle ,false);
 			window.addEventListener('load', sections.toggle ,false);
->>>>>>> master
+
+			window.addEventListener('hashchange', sections.toggle ,false);
+			window.addEventListener('load', sections.toggle ,false);
 		}
 	}
 
 	// Uitbreiden door views extern in te laden?
-<<<<<<< HEAD
-=======
 	// Loader.js
->>>>>>> master
 
 	var sections = {
 
 		toggle: function() {
-
-<<<<<<< HEAD
+			
 			// Get the hash of the current url after click
 			var url = window.location.hash;
 
 			if ( url ) {
 
-				// Get the template that matches the url
 				var matchingTemplate = document.querySelector(url);
 
-				// If this templates exists
 				if ( matchingTemplate ) {
 
-					// Get the content from the matching template and use that content in the main html
-=======
 			var url = window.location.hash;
 
 			if ( url ) {
@@ -64,25 +46,16 @@
 
 				if ( matchingTemplate ) {
 
->>>>>>> master
 					main.innerHTML = matchingTemplate.innerHTML;
 
 				} else {
 
-<<<<<<< HEAD
-					// If the template doesn't exists: load the error template
-=======
->>>>>>> master
 					main.innerHTML = document.querySelector('#error').innerHTML;
 
 				}
 
 			} else {
 				
-<<<<<<< HEAD
-				// If the url has no hash(so this is home) -> set the hash to start
-=======
->>>>>>> master
 				window.location.hash = '#home';
 
 			} 
@@ -91,12 +64,6 @@
 	}
 
 	// Initialize
-<<<<<<< HEAD
-	app.init();
-
-})();
-=======
 	APP.init();
 
 })();
->>>>>>> master
