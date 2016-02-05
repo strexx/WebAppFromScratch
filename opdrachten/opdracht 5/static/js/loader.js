3b9d3d8e597ea2bb4c84f2c740a349c8755e133c
Fons Hettema <fons.hettema@hva.nl>
2016-02-05 12:06:08 +0100

    WebAppFromScratch basic architecture

---
+++ opdrachten/opdracht 5/static/js/loader.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+	
+	// Template loader
+	
+	var template = {
+
+		init: function() {
+			loader.load();
+		}
+
+	}
+
+	var loader = function() {
+
+		load: function(list, options) {
+			
+			var self = this;
+			var url = window.location.hash;
+
+		}
+	}
+
+	// Initialize
+	loader.init();
+});
