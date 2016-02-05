b87b4f9f9c678ea7316fce30a37f7e38abcee912
strexx <ffh_93@live.nl>
2016-02-05 14:17:27 +0100

    Update app.js

--- opdrachten/opdracht 5/static/js/app.js
+++ opdrachten/opdracht 5/static/js/app.js
@@ -3,7 +3,7 @@
 
 	var main = document.querySelector('main');
 
-	var app = {
+	var APP = {
 		init: function() {
 
 			routes.init();
@@ -14,45 +14,36 @@
 	// Routing
 	var routes = {
 		init: function() {
-
-			// haschanged event listener
 			window.addEventListener('hashchange', sections.toggle ,false);
-			// also check the page on load
 			window.addEventListener('load', sections.toggle ,false);
-
 		}
 	}
 
 	// Uitbreiden door views extern in te laden?
+	// Loader.js
 
 	var sections = {
 
 		toggle: function() {
 
-			// Get the hash of the current url after click
 			var url = window.location.hash;
 
 			if ( url ) {
-
-				// Get the template that matches the url
+				
 				var matchingTemplate = document.querySelector(url);
 
-				// If this templates exists
 				if ( matchingTemplate ) {
 
-					// Get the content from the matching template and use that content in the main html
 					main.innerHTML = matchingTemplate.innerHTML;
 
 				} else {
 
-					// If the template doesn't exists: load the error template
 					main.innerHTML = document.querySelector('#error').innerHTML;
 
 				}
 
 			} else {
 				
-				// If the url has no hash(so this is home) -> set the hash to start
 				window.location.hash = '#home';
 
 			} 
@@ -61,6 +52,6 @@
 	}
 
 	// Initialize
-	app.init();
+	APP.init();
 
 })();
