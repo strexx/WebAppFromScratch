72ec6214fe94c44429da1d709ca27e6d0838acf2
Fons Hettema <fons.hettema@hva.nl>
2016-02-17 15:56:52 +0100

    First build with animations, pull to refresh and smooth transitions

---
+++ week 3/static/js/client.js
@@ -0,0 +1,39 @@
+// Source https://stackoverflow.com/questions/247483/http-get-request-in-javascript
+
+var HttpClient = function() { // this is a constructor
+
+    this.get = function (url) {
+        return new Promise((resolve, reject) => {
+            
+            var request = new XMLHttpRequest();
+            
+            // Open an get request
+            request.open('GET', url);
+
+            request.onloadstart = function () {
+                
+                // Toggle animation
+                document.querySelector('main').classList.remove('grow');
+            };
+
+            // If the request is done
+            request.onload = function() {
+                
+                // Only if request is done
+                if (request.status == 200) {
+                    
+                    document.querySelector('main').classList.add('grow');
+                    
+                    // Send text form request
+                    resolve(request.responseText);
+                } else {
+                    // Reject the promise if there is a err
+                    reject(new Error('Request failed!'));
+                }
+            };    
+
+            // Send the request
+            request.send();
+        });
+    }
+}
